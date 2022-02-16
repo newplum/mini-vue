4ffd9f02ec28640f6a48e268824da39fb95822e4
newplum <1105372736@qq.com>
2022-02-16 22:52:34 +0800

    effect stop

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,15 +1,37 @@
+import { extend } from '../share'
+
 class ReactiveEffect {
-  constructor(private _fn: any) {}
+  deps = []
+  active = true
+  onStop?: () => void
+  constructor(private _fn: any, public scheduler) {}
   run() {
     activeEffect = this
-    this._fn()
+    return this._fn()
+  }
+  stop() {
+    if (this.active) {
+      cleanupEffect(this)
+      this.active = false
+      this.onStop && this.onStop()
+    }
   }
 }
 
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect)
+  })
+}
+
 let activeEffect
-export function effect(fn) {
-  const effect = new ReactiveEffect(fn)
+export function effect(fn, options: any = {}) {
+  const effect = new ReactiveEffect(fn, options.scheduler)
+  extend(effect, options)
   effect.run()
+  const runner: any = effect.run.bind(effect)
+  runner.effect = effect
+  return runner
 }
 
 const targetMap = new Map()
@@ -24,13 +46,23 @@ export function track(target, key) {
     dep = new Set()
     depsMap.set(key, dep)
   }
+  if (!activeEffect) return
   dep.add(activeEffect)
+  activeEffect.deps.push(dep)
 }
 
 export function trigger(target, key) {
   const depsMap = targetMap.get(target)
   const dep = depsMap.get(key)
   for (const effect of dep) {
-    effect.run()
+    if (effect.scheduler) {
+      effect.scheduler()
+    } else {
+      effect.run()
+    }
   }
+}
+
+export function stop(runner) {
+  runner.effect.stop()
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -1,4 +1,4 @@
-import { effect } from '../effect'
+import { effect, stop } from '../effect'
 import { reactive } from '../reactive'
 describe('effect', () => {
   it('happy path', () => {
@@ -12,5 +12,74 @@ describe('effect', () => {
     expect(b).toBe(2)
     obj.a++
     expect(b).toBe(3)
+  })
+
+  it('should return runner when call effect', () => {
+    let foo = 1
+    const runner = effect(() => {
+      foo++
+      return 'foo'
+    })
+    expect(foo).toBe(2)
+    const result = runner()
+    expect(foo).toBe(3)
+    expect(result).toBe('foo')
+  })
+
+  it('scheduler', () => {
+    let obj = reactive({
+      a: 1,
+    })
+    let foo
+    let run
+    const scheduler = jest.fn(() => {
+      run = runner
+    })
+    const runner = effect(
+      () => {
+        foo = obj.a
+      },
+      { scheduler }
+    )
+    expect(scheduler).not.toHaveBeenCalled()
+    expect(foo).toBe(1)
+    obj.a++
+    expect(scheduler).toHaveBeenCalledTimes(1)
+    expect(foo).toBe(1)
+    run()
+    expect(foo).toBe(2)
+  })
+
+  it('stop', () => {
+    const obj = reactive({
+      foo: 1,
+    })
+    let dummy
+    const runner = effect(() => {
+      dummy = obj.foo
+    })
+    obj.foo = 2
+    expect(dummy).toBe(2)
+    stop(runner)
+    obj.foo = 3
+    expect(dummy).toBe(2)
+    runner()
+    expect(dummy).toBe(3)
+  })
+
+  it('onStop', () => {
+    const obj = reactive({ foo: 1 })
+    let dummy
+    const onStop = jest.fn(() => {})
+    const runner = effect(
+      () => {
+        dummy = obj.foo
+      },
+      {
+        onStop,
+      }
+    )
+    stop(runner)
+    expect(onStop).toBeCalledTimes(1)
   })
 })
